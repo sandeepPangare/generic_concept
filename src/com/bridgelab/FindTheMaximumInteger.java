6f3d3f4f197a4138cde11218d59ae6fd1a3a5dad
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-11 15:39:02 +0530

    Added:FindTheMaximumInteger

---
+++ src/com/bridgelab/FindTheMaximumInteger.java
@@ -0,0 +1,19 @@
+package com.bridgelab;
+
+public class FindTheMaximumInteger {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Integers Problem using Generics: ");
+        Integer a = 89, b =99, c = 29;//variables
+        testMaximum (a,b,c);//calling method
+    }
+    private static void testMaximum(Integer a,Integer b, Integer c){
+        Integer max =a;//checking the condition
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max =c;
+        }
+        System.out.println("Maximum Number is "+max);
+    }
+}
